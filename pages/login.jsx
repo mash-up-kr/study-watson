f1505acd290f67a87697510cc81dd24cb4ebe604
leesoo <isoo7510@gmail.com>
2019-06-29 21:05:12 +0900

    feature login

---
+++ pages/login.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import styled from "styled-components";
+
+const Index = () => {
+
+  const [click, isClicked] = useState(false);
+
+  const goLogin = () => {
+    isClicked(true);
+    console.log("구글로 로그인하러 가기");
+  };
+
+  return (
+    <div>
+      <div>로그인</div>
+      <Btn onClick={goLogin}>Google로 시작하기</Btn>
+    </div>
+  )
+};
+
+const Btn = styled.button`
+  background-color: blue;
+  color: white;
+`;
+
+export default Index;
