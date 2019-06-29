a85733446e6800ee82019f55f1d2469e682717d8
Yuni-Q <carnotcycle2@naver.com>
2019-06-30 03:48:39 +0900

    fix: fix withdraw

--- pages/withdraw.jsx
+++ pages/withdraw.jsx
@@ -4,12 +4,17 @@ import { useDispatch } from 'react-redux';
 import Router from 'next/router';
 
 import Header from '../components/Header';
-import { WITHDRAW } from '../reducers/user'
+import { WITHDRAW } from '../reducers/user';
+
+const StyledWithdraw = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 const StyledButton = styled.div`
   width: 100%;
   padding: 1rem 0;
-  background-color: #0077FF;
+  background-color: #0077ff;
   font-size: 1rem;
   color: #fff;
   text-align: center;
@@ -17,34 +22,32 @@ const StyledButton = styled.div`
   bottom: 0;
 `;
 
-const Index = () => {
+const Withdraw = () => {
   const dispatch = useDispatch();
 
   const onClick = () => {
     dispatch({
       type: WITHDRAW,
       isLogin: false,
-    })
+    });
     Router.pushRoute('/');
-  }
+  };
 
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
+    <StyledWithdraw>
       <Header />
       <h2>탈퇴하기</h2>
       <ul>
         <li>･ 참여 중인 스터디 정보가 모두 삭제됩니다.</li>
         <li>･ 만든 스터디가 있다면 스터디 장을 위임해주세요.</li>
-        <li>･ 회원 탈퇴 시 계정 정보가 모두 삭제되며 삭제된 계정은 복구되지 않습니다.</li>
+        <li>
+          ･ 회원 탈퇴 시 계정 정보가 모두 삭제되며 삭제된 계정은 복구되지
+          않습니다.
+        </li>
       </ul>
       <StyledButton onClick={onClick}>회원탈퇴</StyledButton>
-    </div>
+    </StyledWithdraw>
   );
 };
 
-export default Index;
+export default Withdraw;
