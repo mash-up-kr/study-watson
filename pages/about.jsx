b7e13fb2c41d62258f51fb2d73fbc2161dddda5d
snaag <04_miffy@naver.com>
2019-07-07 17:16:24 +0900

    add: 알아보기 page

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../containers/Header';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <h1>알아보기 페이지 입니다</h1>
+    </div>
+  );
+};
+
+export default About;
