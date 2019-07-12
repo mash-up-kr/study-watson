0b8318b3e3ed4e3b1f0aab7abf7c00afa0f4ceed
Yuni-Q <carnotcycle2@naver.com>
2019-07-12 19:02:38 +0900

    refactor: reafactor CreateStudy
    Fixes #14

--- containers/CreateStudy.jsx
+++ containers/CreateStudy.jsx
@@ -1,49 +1,66 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import uuidv4 from 'uuid/v4';
 
 import StudyInvite from './StudyInvite';
 import ShowStudy from '../components/ShowStudy';
 import MakeStudy from '../components/MakeStudy';
 import { useInput } from '../common/useInput';
 import { ADD_STUDY } from '../reducers/study';
+import { canUseDOM } from '../common/canUesDOM';
+
+const firstPage = 'firstPage';
+const secondPage = 'secondPage';
+const thirdPage = 'thirdPage';
 
 const CreateStudy = () => {
-  const id = uuidv4();
-  const [depth, setDepth] = useState(0);
+  const [page, setPage] = useState(firstPage);
   const [title, setTitle] = useInput('');
   const [description, setDescription] = useInput('');
 
   const dispatch = useDispatch();
 
-  const addStudy = to => {
+  const link = canUseDOM() && `${window.location.origin}/join/id`;
+
+  const addStudy = () => {
     dispatch({
       type: ADD_STUDY,
-      payload: { id, title, description },
+      payload: { title, description },
     });
-    setDepth(to);
+    setPage(secondPage);
   };
 
-  const onSubmitForm = (e, to) => {
+  const onSubmitForm = e => {
     e.preventDefault();
-    addStudy(to);
+    addStudy();
   };
 
-  const components = [
-    <MakeStudy
-      onSubmitForm={onSubmitForm}
-      title={title}
-      setTitle={setTitle}
-      setDescription={setDescription}
-      description={description}
-    />,
-    <ShowStudy title={title} description={description} setDepth={setDepth} />,
-    <StudyInvite link={id} />,
-  ];
+  const moveThirdPage = () => {
+    setPage(thirdPage);
+  };
+
+  const components = {
+    [firstPage]: (
+      <MakeStudy
+        onSubmitForm={onSubmitForm}
+        title={title}
+        setTitle={setTitle}
+        setDescription={setDescription}
+        description={description}
+      />
+    ),
+    [secondPage]: (
+      <ShowStudy
+        title={title}
+        description={description}
+        moveThirdPage={moveThirdPage}
+      />
+    ),
+    [thirdPage]: <StudyInvite link={link} />,
+  };
 
   return (
     // MakeStudy - ShowStudy - StudyInvite
-    <div>{components[depth]}</div>
+    <div>{components[page]}</div>
   );
 };
 
