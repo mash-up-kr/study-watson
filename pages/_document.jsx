be2cfbb6a5e10e01edbf48a30b89ae9f1ec2987a
Yuni-Q <carnotcycle2@naver.com>
2019-07-14 09:58:17 +0900

    feat: add gtm
    Fixes #42

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -169,11 +169,34 @@ export default class CustomDocument extends Document {
     return (
       <html lang="ko" {...htmlAttrs}>
         <head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-143821755-1"
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MPFSK4P');
+            `,
+            }}
+          />
           {this.props.styleTags}
           {Object.values(helmet).map(el => el.toComponent())}
           <GlobalStyle />
         </head>
         <body {...bodyAttrs}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5KMDZP4"
+              style={{
+                display: 'none',
+                visibility: 'hidden',
+                width: '0',
+                height: '0',
+              }}
+            />
+          </noscript>
           <Main />
           {process.env.NODE_ENV === 'production' && (
             <script src="https://polyfill.io/v3/polyfill.min.js?features=es6,es7,es8,es9,NodeList.prototype.forEach&flags=gated" />
