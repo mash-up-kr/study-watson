eb2b28c6c0ea056c19edc8dd94ce0b47760981b6
Yuni-Q <carnotcycle2@naver.com>
2019-07-17 12:25:20 +0900

    hotfix: fix service-worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,27 +3,31 @@
 // Install stage sets up the offline page in the cache and opens a new cache
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('install', event => {
-  console.log(11, event);
   const offlinePage = new Request('/');
   event.waitUntil(
     fetch(offlinePage).then(response => {
-      return caches.open('pwabuilder-offline').then(cache => {
-        console.log(`Page cached ${response.url}`);
-        return cache.put(offlinePage, response);
-      });
+      return caches
+        .open('study-watson-offline')
+        .then(cache => {
+          console.log(`Page cached ${response.url}`);
+          return cache.put(offlinePage, response);
+        })
+        .catch(error => console.log('실패', error));
     }),
   );
 });
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('fetch', event => {
-  console.log(event.request.url);
   event.respondWith(
     fetch(event.request).catch(error => {
       console.error(`Serving Offline ${error}`);
-      return caches.open('pwabuilder-offline').then(cache => {
-        return cache.match('/');
-      });
+      return caches
+        .open('study-watson-offline')
+        .then(cache => {
+          return cache.match('/');
+        })
+        .catch(error => console.log('실패', error));
     }),
   );
 });
