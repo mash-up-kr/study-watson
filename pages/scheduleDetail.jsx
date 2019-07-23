0f5844e96e4a068dd7d76a9c50663b04117b1c97
Yuni-Q <carnotcycle2@naver.com>
2019-07-23 11:01:16 +0900

    refactor: refactor scheduleDetail
    Fixes #85

--- pages/scheduleDetail.jsx
+++ pages/scheduleDetail.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
-import Router from 'next/router';
 import Axios from 'axios';
 
 import Header from '../containers/Header';
@@ -9,40 +7,85 @@ import Attendance from '../components/Attendance';
 import { Link } from '../routes';
 import { LOAD_SCHEDULE_REQUEST } from '../reducers/schedule';
 
-const ScheduleDetail = ({ scheduleId }) => {
+const ScheduleDetail = () => {
+  const mount = useRef(null);
   const { schedule } = useSelector(state => state.schedule);
-  const attend = [];
-  const late = [];
-  const absent = [];
-  const none = [];
+  const [attend, setAttend] = useState([]);
+  const [late, setLate] = useState([]);
+  const [absent, setAbsent] = useState([]);
+  const [none, setNone] = useState([]);
   if (schedule.attendanceSet && schedule.attendanceSet.length > 0) {
+    const attendList = [];
+    const lateList = [];
+    const absentList = [];
+    const noneList = [];
     schedule.attendanceSet.forEach(attendance => {
       if (attendance.att === 'attend') {
-        attend.push(attendance);
+        attendList.push(attendance);
       } else if (attendance.att === 'late') {
-        late.push(attendance);
+        lateList.push(attendance);
       } else if (attendance.att === 'absent') {
-        absent.push(attendance);
+        absentList.push(attendance);
       } else {
-        none.push(attendance);
+        noneList.push(attendance);
       }
     });
+    if (!mount.current) {
+      mount.current = true;
+      setAttend(attendList);
+      setLate(lateList);
+      setAbsent(absentList);
+      setNone(noneList);
+    }
   }
 
   const onClickAttendance = async event => {
+    const { pk, user, attendance: att } = event.target.dataset;
     try {
       await Axios.patch(
-        `https://study-watson.lhy.kr/api/v1/study/attendances/${
-          event.target.dataset.pk
-        }/`,
+        `https://study-watson.lhy.kr/api/v1/study/attendances/${pk}/`,
         {
-          user: event.target.dataset.user,
-          att: event.target.dataset.attendance,
+          user,
+          att,
         },
       );
-      Router.push(`/schedule/${scheduleId}`);
+      // Router.push(`/schedule/${scheduleId}`);
+      const attendance =
+        attend.find(Element => {
+          return JSON.parse(Element.pk) === JSON.parse(pk);
+        }) ||
+        late.find(Element => {
+          return JSON.parse(Element.pk) === JSON.parse(pk);
+        }) ||
+        absent.find(Element => {
+          return JSON.parse(Element.pk) === JSON.parse(pk);
+        });
+      const filterAttend = attend.filter(Element => {
+        return JSON.parse(Element.pk) !== JSON.parse(pk);
+      });
+      setAttend(filterAttend);
+      const filterLate = late.filter(Element => {
+        return JSON.parse(Element.pk) !== JSON.parse(pk);
+      });
+      setLate(filterLate);
+      const filterAbsent = absent.filter(Element => {
+        return JSON.parse(Element.pk) !== JSON.parse(pk);
+      });
+      setAbsent(filterAbsent);
+      if (att === 'attend') {
+        // eslint-disable-next-line no-shadow
+        setAttend(attend => [...attend, attendance]);
+      } else if (att === 'late') {
+        // eslint-disable-next-line no-shadow
+        setLate(late => [...late, attendance]);
+      } else if (att === 'absent') {
+        // eslint-disable-next-line no-shadow
+        setAbsent(absent => [...absent, attendance]);
+      } else {
+        console.log('???');
+      }
     } catch (error) {
-      console.log(error.response.data);
+      console.log(error);
     }
   };
 
@@ -116,11 +159,6 @@ ScheduleDetail.getInitialProps = ({ ctx, token }) => {
       token,
     },
   });
-  return { scheduleId };
-};
-
-ScheduleDetail.propTypes = {
-  scheduleId: PropTypes.string.isRequired,
 };
 
 export default ScheduleDetail;
