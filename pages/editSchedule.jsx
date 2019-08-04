bd125ffcd7b74952a5a1e9c37ce1fa98362c87ec
snaag <04_miffy@naver.com>
2019-08-04 18:44:42 +0900

    feat: 일정 수정 구현

--- pages/editSchedule.jsx
+++ pages/editSchedule.jsx
@@ -1,93 +1,100 @@
 import React, { useEffect } from 'react';
 // import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Header from '../containers/Header';
 import { useInput } from '../common/useInput';
 import Input from '../components/Input';
-import { LOAD_SCHEDULE_REQUEST } from '../reducers/schedule';
+import {
+  LOAD_SCHEDULE_REQUEST,
+  UPDATE_SCHEDULE_REQUEST,
+} from '../reducers/schedule';
 
 const editSchedule = () => {
   const { schedule } = useSelector(state => state.schedule);
 
-  const [subject, setSubject] = useInput(schedule.subject);
+  // const [subject, setSubject] = useInput(schedule.subject);
   const [location, setLocation] = useInput(schedule.location);
   const [description, setDescription] = useInput(schedule.description);
   const [voteEndAt, setvoteEndAt] = useInput(schedule.voteEndAt.slice(0, 19));
   const [startAt, setStartAt] = useInput(schedule.startAt.slice(0, 19));
-  const [studyingTime, setStudyTime] = useInput(schedule.studyingTime);
+  const [studyingTime, setStudyTime] = useInput(
+    schedule.studyingTime.slice(0, 5),
+  );
 
-  // useEffect(() => {
-  //   const s = {
-  //     target: {
-  //       value: schedule.subject,
-  //     },
-  //   };
-  //   setSubject(s);
-  //   const l = {
-  //     target: {
-  //       value: schedule.location,
-  //     },
-  //   };
-  //   setLocation(l);
-  //   const d = {
-  //     target: {
-  //       value: schedule.description,
-  //     },
-  //   };
-  //   setDescription(d);
-  //   const v = {
-  //     target: {
-  //       value: schedule.voteEndAt.slice(0, 19),
-  //     },
-  //   };
-  //   setvoteEndAt(v);
-  //   const sa = {
-  //     target: {
-  //       value: schedule.startAt.slice(0, 19),
-  //     },
-  //   };
-  //   setStartAt(sa);
-  //   const st = {
-  //     target: {
-  //       value: schedule.studyingTime,
-  //     },
-  //   };
-  //   setStudyTime(st);
-  // }, [schedule]);
+  useEffect(() => {
+    // const s = {
+    //   target: {
+    //     value: schedule.subject,
+    //   },
+    // };
+    // setSubject(s);
+    const l = {
+      target: {
+        value: schedule.location,
+      },
+    };
+    setLocation(l);
+    const d = {
+      target: {
+        value: schedule.description,
+      },
+    };
+    setDescription(d);
+    const v = {
+      target: {
+        value: schedule.voteEndAt.slice(0, 19),
+      },
+    };
+    setvoteEndAt(v);
+    const sa = {
+      target: {
+        value: schedule.startAt.slice(0, 19),
+      },
+    };
+    setStartAt(sa);
+    const st = {
+      target: {
+        value: schedule.studyingTime.slice(0, 5),
+      },
+    };
+    setStudyTime(st);
+  }, [schedule]);
+
+  const dispatch = useDispatch();
 
   const onSumit = event => {
     event.preventDefault();
-    // const voteEndAtToISOString = new Date(voteEndAt).toISOString();
+    const voteEndAtToISOString = new Date(voteEndAt).toISOString();
     const startAtToISOString = new Date(startAt).toISOString();
-    console.log(111, startAt, schedule.startAt, startAtToISOString);
 
-    // dispatch({
-    //   type: ADD_SCHEDULE_REQUEST,
-    //   data: {
-    //     study: studyId,
-    //     subject,
-    //     location,
-    //     description,
-    //     voteEndAt: voteEndAtToISOString,
-    //     startAt: startAtToISOString,
-    //     studyingTime: `${studyingTime}:00`,
-    //   },
-    // });
+    dispatch({
+      type: UPDATE_SCHEDULE_REQUEST,
+      data: {
+        id: schedule.pk,
+        study: schedule.study,
+        // subject,
+        location,
+        description,
+        voteEndAt: voteEndAtToISOString,
+        startAt: startAtToISOString,
+        studyingTime: `${studyingTime}:00`,
+      },
+    });
   };
 
   return (
     <>
       <Header />
       <form onSubmit={onSumit}>
-        <Input
+        {/* <Input
           label="주제"
           id="subject"
           type="text"
           value={subject}
           onChange={setSubject}
           onClickReset={() => setSubject('')}
-        />
+        /> */}
         <Input
           label="장소"
           id="location"
@@ -128,7 +135,7 @@ const editSchedule = () => {
           onChange={setStudyTime}
           onClickReset={() => setStudyTime('')}
         />
-        <input type="submit" value="생성" />
+        <input type="submit" value="저장" />
       </form>
     </>
   );
