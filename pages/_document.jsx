c54cd5cd19a86011b8f996230312edd6de8067f5
Yuni-Q <carnotcycle2@naver.com>
2019-08-21 17:41:31 +0900

    feat: add reset css
    Fixes #224

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -31,7 +31,11 @@ export default class CustomDocument extends Document {
     return (
       <html lang="ko" dir="ltr" {...htmlAttrs}>
         <head>
-          <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap"
+            rel="stylesheet"
+          />
+          <link href="/static/reset.css" rel="stylesheet" />
           <GlobalStyle />
           <script
             async
