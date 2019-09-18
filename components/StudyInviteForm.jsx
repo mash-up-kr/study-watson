58c840671b0abc82bbc01162d579cb79b2f8305a
Yuni-Q <carnotcycle2@naver.com>
2019-09-18 19:22:17 +0900

    refactor stduyInvite

---
+++ components/StudyInviteForm.jsx
@@ -0,0 +1,116 @@
+import React, { useCallback, useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import {
+  StyledButton,
+  StyledImageText,
+  StyledActionButton,
+} from '../common/StyledComponents';
+import { Link } from '../routes';
+
+const StyledContainer = styled.div`
+  width: calc(100vw - 2rem);
+  height: calc(100vh - 110px);
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const StyledLink = styled.input`
+  border: 1px solid #ededed;
+  width: 100%;
+  padding: 1rem;
+  font-size: 0.9rem;
+  margin-bottom: 1.5rem;
+  text-align: center;
+  border-radius: 8px;
+  color: #4D5256;
+`;
+
+
+const StyledToast = styled.div`
+  width: 100%;
+  padding: 1rem 0;
+  background-color: #595959;
+  color: #fff;
+  position: fixed;
+  bottom: 3rem;
+  font-size: 0.9rem;
+  text-align: center;
+`;
+
+const StudyInviteForm = ({ studyId, token }) => {
+  const [show, setShow] = useState(false);
+  const [url, setUrl] = useState('링크를 생성해주세요');
+
+  const getUrl = useCallback(async () => {
+    const result = await axios.post(
+      'https://study-watson.lhy.kr/api/v1/study/invite-token/',
+      {
+        study: studyId,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${token}`,
+        },
+      },
+    );
+    const value = `${window.location.host}/join/${result.data.key}`;
+    setUrl(value);
+  }, []);
+
+  const clickLink = useCallback(async () => {
+    await getUrl();
+  }, []);
+
+  const onClickLink = useCallback(() => {
+    document.querySelector('#url').select();
+    document.execCommand('copy');
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <StyledContainer>
+      <img
+        src="/static/image_invite.svg"
+        alt="invite illustration"
+        style={{ marginBottom: '1rem' }}
+      />
+      <StyledImageText>
+        링크를 공유해서
+          <br />
+        스터디원을 초대해보세요!
+        </StyledImageText>
+      <StyledLink
+        type="text"
+        value={url}
+        id="url"
+        onClick={onClickLink}
+        readOnly
+      />
+      <StyledActionButton onClick={clickLink} name="make" type="button" value="링크 생성" />
+      {show && <StyledToast>링크가 복사되었습니다!</StyledToast>}
+      <br />
+      <Link route="/" href="/">
+        <a>
+          <StyledButton type="button" value="홈으로 돌아가기" />
+        </a>
+      </Link>
+    </StyledContainer>
+  );
+};
+
+StudyInviteForm.propTypes = {
+  studyId: PropTypes.string.isRequired,
+  token: PropTypes.string.isRequired,
+};
+
+export default StudyInviteForm;
